9c8c078503c071159a4a81ba0c937ffd7a4eaa31
Elioth1 <31458423+Elioth1@users.noreply.github.com>
2017-12-03 16:43:05 -0600

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace cesar
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Cesar}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
